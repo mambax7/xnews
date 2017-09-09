bb78efa9a21e30aa5e8f5cd64ec60348f4e84cf4
mambax7 <mambax7@gmail.com>
2017-09-09 09:59:42 -0400

    menu standardization

--- extra/modules/xnewsimport/admin/menu.php
+++ extra/modules/xnewsimport/admin/menu.php
@@ -25,25 +25,27 @@ if (false !== ($moduleHelper = Xmf\Module\Helper::getHelper($moduleDirName))) {
     $moduleHelper = Xmf\Module\Helper::getHelper('system');
 }
 
-
 $pathIcon32 = \Xmf\Module\Admin::menuIconPath('');
 //$pathModIcon32 = $moduleHelper->getModule()->getInfo('modicons32');
 
 $moduleHelper->loadLanguage('modinfo');
 
-$adminmenu              = [];
-$i                      = 0;
-$adminmenu[$i]['title'] = _AM_MODULEADMIN_HOME;
-$adminmenu[$i]['link']  = 'admin/index.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/home.png';
-++$i;
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_HOME,
+    'link'  => 'admin/index.php',
+    'icon'  => $pathIcon32 . '/home.png',
+];
 
-$adminmenu[$i]['title']      = _MI_XNI_IMPORT;
-$adminmenu[$i]['link']       = 'admin/main.php?op=import';
-$adminmenu[$i]['icon']       = 'images/import32.png';
-$adminmenu[$i]['icon_small'] = 'images/import32.png';
+$adminmenu[] = [
+    'title'      => _MI_XNI_IMPORT,
+    'link'       => 'admin/main.php?op=import',
+    'icon'       => 'images/import32.png',
+    'icon_small' => 'images/import32.png',
+];
 
-++$i;
-$adminmenu[$i]['title'] = _AM_MODULEADMIN_ABOUT;
-$adminmenu[$i]['link']  = 'admin/about.php';
-$adminmenu[$i]['icon']  = $pathIcon32 . '/about.png';
+$adminmenu[] = [
+    'title' => _AM_MODULEADMIN_ABOUT,
+    'link'  => 'admin/about.php',
+    'icon'  => $pathIcon32 . '/about.png',
+];
+
